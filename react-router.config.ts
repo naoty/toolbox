154c012b332615c2aeaa808c2233407ab368f737
Naoto Kaneko <naoty.k@gmail.com>
2026-01-16 20:46:34 +0900

    pre-renderの設定にも末尾スラッシュをつける

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   async prerender() {
-    return ["/", "/count", "/random-text"];
+    return ["/", "/count/", "/random-text/"];
   },
 } satisfies Config;
